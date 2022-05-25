95881e89f2f7ff0e681fbb1f8c5d204a9b6d4114
DiEvil21 <55885322+DiEvil21@users.noreply.github.com>
2022-05-25 17:42:58 +0300

    Add files via upload

---
+++ osi/blocks/people.php
@@ -0,0 +1,17 @@
+<div class="row">
+      <div class="col-lg-4">
+        <img src="assets/img/dima.jpg" alt="" height="140" style="border-radius: 90px;">
+
+        <h2 class="fw-normal">Ярулин Дмитрий</h2>
+        <p>Специалист, функционалист</p>
+        
+      </div><!-- /.col-lg-4 -->
+      <div class="col-lg-4">
+        <img src="assets/img/timur.jpg" alt="" height="140" style="border-radius: 90px;">
+
+        <h2 class="fw-normal">Журавли Тимур</h2>
+        <p>Менеджер, коммуникатор, аналитик</p>
+        
+      </div><!-- /.col-lg-4 -->
+      
+    </div>
---
+++ osi/blocks/portfolio.php
@@ -0,0 +1,17 @@
+<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg background-white">
+      <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1">Консультация с менеджером</h1>
+        <p class="lead">Обсудите все условия реализации в вашем вузе. Вам обязательно помогут.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <a href="alert.html" class="a-class">
+          <button  type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Оставить заявку</button>
+          </a>
+          
+        </div>
+      </div>
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden p-3">
+          <img class="rounded-lg-3" src="assets/img/manageback.png" alt="" width="240">
+      </div>
+    </div>
+
+
